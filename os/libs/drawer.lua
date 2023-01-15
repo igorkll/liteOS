2507d3309e8d834922bfc61f080b13712da5d029
igorkll <frerther@gmail.com>
2023-01-15 14:51:26 +0300

    Update drawer.lua

--- os/libs/drawer.lua
+++ os/libs/drawer.lua
@@ -1,28 +1,70 @@
 local drawer = {}
 
-function drawer:create()
+function drawer.create() --создает графическую системму, состоящию из видеокарты и монитора
+    local gpu = component.proxy(component.list("gpu")() or "")
+    local screen = component.list("screen")()
+
+    if gpu and screen then
+        local obj = setmetatable({
+            gpu = gpu,
+            screen = screen
+        }, {_index = drawer})
+
+        gpu.bind(screen)
+        local rx, ry = gpu.maxResolution()
+
+        gpu.setDepth(1) --сброс палитры
+        gpu.setDepth(gpu.maxDepth())
+
+        gpu.setResolution(rx, ry) --ставим максимальное разрешения
+
+        gpu.setBackground(0) --цвета по умалчанию
+        gpu.setForeground(0xFFFFFF)
+
+        gpu.fill(1, 1, rx, ry, " ") --очистка экрана
+
+        obj.depth = gpu.getDepth()
+        obj.sizeX = rx
+        obj.sizeY = ry
+
+        ------------------------------------
+
+        if obj.depth > 1 then
+            obj.palette = {}
+            for i = 0, 15 do
+                obj.palette[i] = gpu.getPaletteColor(i)
+            end
+        end
+
+        return obj
+    end
+end
+
+------------------------------------------------------------------------
+
+function drawer:begin_draw()
+    if self.hardwareBuffer then
+        self.gpu.setActiveBuffer(self.hardwareBuffer)
+    end
+end
+
+function drawer:end_draw()
+    if self.hardwareBuffer then
+        self.gpu.bitblt()
+    end
+end
+
+------------------------------------------------------------------------
+
+function drawer:set()
     
 end
 
-function drawer:init()--инициализация графической системмы
-    self.gpu.bind(self.screen)
-    local rx, ry = self.gpu.maxResolution()
-
-    if self.gpu.setActiveBuffer then --если версия open computers поддерживает буферы
-        self.gpu.setActiveBuffer(0) --лутще их удалить, малоли что там будет
-        self.gpu.freeAllBuffers()
-    end
-
-    self.gpu.setDepth(1) --сброс палитры
-    self.gpu.setDepth(self.gpu.maxDepth())
-
-    self.gpu.setResolution(rx, ry) --ставим максимальное разрешения
-
-    self.gpu.setBackground(0) --цвета по умалчанию
-    self.gpu.setForeground(0xFFFFFF)
-
-    self.gpu.fill(1, 1, rx, ry, " ") --очистка экрана
+function drawer:fill()
+    
 end
+
+
 
 
 return drawer
