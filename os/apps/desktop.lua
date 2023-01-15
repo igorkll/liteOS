b849b8de85ea49bf9128ea9494c6314f6c470b4b
igorkll <frerther@gmail.com>
2023-01-15 19:54:37 +0300

    Update desktop.lua

--- os/apps/desktop.lua
+++ os/apps/desktop.lua
@@ -1,12 +1,12 @@
 local drawer = require("drawer")
-local drawzone = drawer:create()
+local drawzone = drawer:create({})
 
 while true do
     drawzone:draw_begin()
     drawzone:clear(0xFFFF00)
-    drawzone:fill(2, 2, 16, 16, 0xFF00FF, 0x00FF00, "@")
+    drawzone:fill(2, ((computer.uptime() * 16) % 20) + 2, 16, 16, 0xFF00FF, 0x00FF00, "@")
     drawzone:set(1, 1, 0xFFFFFF, 0xFF0000, "A")
     drawzone:draw_end()
 
-    os.sleep(1)
+    os.sleep(0.1)
 end 
