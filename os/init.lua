949b9445ba69c88b4ff91c53a9361e3be859e899
igorkll <frerther@gmail.com>
2023-01-15 19:32:57 +0300

    u

--- os/init.lua
+++ os/init.lua
@@ -35,15 +35,4 @@ require("utilites")
 
 ---------------------------------------------------
 
-local drawer = require("drawer")
-local drawzone = drawer:create()
-
-while true do
-    drawzone:draw_begin()
-    drawzone:clear(0xFFFF00)
-    drawzone:fill(2, 2, 16, 16, 0xFF00FF, 0x00FF00, "@")
-    drawzone:set(1, 1, 0xFFFFFF, 0xFF0000, "A")
-    drawzone:draw_end()
-
-    os.sleep(1)
-end 
+require("programs").run("desktop")
