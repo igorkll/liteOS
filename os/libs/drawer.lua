54622443fd4cd116451408010af4f9c43056def5
igorkll <frerther@gmail.com>
2023-01-17 16:15:37 +0300

    u

--- os/libs/drawer.lua
+++ os/libs/drawer.lua
@@ -20,22 +20,22 @@ drawer.palette_defaultTier2 = {[0] = 16777215.0,16763955.0,13395660.0,6724095.0,
 drawer.palette_defaultTier3 = {[0] = 986895.0,1973790.0,2960685.0,3947580.0,4934475.0,5921370.0,6908265.0,7895160.0,8882055.0,9868950.0,10855845.0,11842740.0,12829635.0,13816530.0,14803425.0,15790320.0}
 
 drawer.palette_computercraft = { --цвета computercraft
-    0xF0F0F0, --1)  white
-    0xF2B233, --2)  orange
-    0xE57FD8, --3)  magenta
-    0x99B2F2, --4)  lightBlue
-    0xDEDE6C, --5)  yellow
-    0x7FCC19, --6)  lime
-    0xF2B2CC, --7)  pink
-    0x4C4C4C, --8)  gray
-    0x999999, --9)  lightGray
-    0x4C99B2, --10) cyan
-    0xB266E5, --11) purple
-    0x3366CC, --12) blue
-    0x7F664C, --13) brown
-    0x57A64E, --14) green
-    0xCC4C4C, --15) red
-    0x191919  --16) black
+    [0] = 0x191919,
+    0xCC4C4C,
+    0x57A64E,
+    0x7F664C,
+    0x3366CC,
+    0xB266E5,
+    0x4C99B2,
+    0x999999,
+    0x4C4C4C,
+    0xF2B2CC,
+    0x7FCC19,
+    0xDEDE6C,
+    0x99B2F2,
+    0xE57FD8,
+    0xF2B233,
+    0xF0F0F0,
 }
 
 
@@ -122,7 +122,8 @@ function drawer.create(settings) --создает графическую сис�
             end
         end
         
-
+        obj.flushed = true
+        
         obj.settings = settings
         return obj
     end
@@ -208,7 +209,8 @@ end
 
 function drawer:draw_end()
     if self.softwareBuffer then
-        self.softwareBuffer.update()
+        self.softwareBuffer.update(self.flushed)
+        self.flushed = nil
     end
     if self.hardwareBuffer then
         self.gpu.bitblt()
@@ -244,7 +246,7 @@ function drawer:fill(x, y, sx, sy, bg, fg, char)
     if self.softwareBuffer then
         for cx = x, x + (sx - 1) do
             for cy = y, y + (sy - 1) do
-                self.softwareBuffer.set(cx, cy, bg or 0, fg or self.maxFg, char)
+                self.softwareBuffer.set(cx, cy, bg or 0, fg or self.maxFg, char or " ")
             end
         end
     else
@@ -255,12 +257,7 @@ function drawer:fill(x, y, sx, sy, bg, fg, char)
 end
 
 function drawer:clear(bg, fg, char)
-    if self.softwareBuffer then
-        self:fill(1, 1, self.sizeX, self.sizeY, bg, fg, char or " ")
-    else
-        self:_setColor(bg, fg)
-        self.gpu.fill(1, 1, self.sizeX, self.sizeY, char or " ")
-    end
+    self:fill(1, 1, self.sizeX, self.sizeY, bg, fg, char)
     return true
 end
 
