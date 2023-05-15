81accd34ab42ed252b01e4163e3f7b5f98048a41
igorkll <frerther@gmail.com>
2023-05-15 12:24:18 +0300

    Update desktop.lua

--- services/desktop.lua
+++ services/desktop.lua
@@ -1,3 +1,5 @@
 local dialogWindows = require("dialogWindows")
+local system = require("system")
+local gui = system.gui
 
 dialogWindows.message(scene, "hello!", "a new os!", gui:getColor("lightGray"))
